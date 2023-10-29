47af34cc3e42296b1a0556a238ab2fe6eaa6b5f6
theKyrylo <zhulin.pers0n4l@gmail.com>
2023-10-29 10:20:52 +0100

    Updating .gitignore and initialization of program functions

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace CSharpConsoleSnake
+{
+    class Program
+    {
+        // Fuction that draws the game field
+        public static void Draw()
+        {
+
+        }
+        // Function fot fruit spawn and updating
+        public static void Fruit()
+        {
+
+        }
+        // Function that is checking what key is pressed
+        public static void Input()
+        {
+
+        }
+        // Function that manages the snake
+        public static void Snake()
+        {
+
+        }
+        // Main function
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
